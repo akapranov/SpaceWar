2b698f37e4bafb3d56fec15596d1694192f18a43
Alexander <ka.ateist@gmail.com>
2018-08-15 13:43:45 +0300

    с лекции

--- core/src/ru/geekbrains/spacewar/screen/MenuScreen.java
+++ core/src/ru/geekbrains/spacewar/screen/MenuScreen.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.spacewar.screen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -13,12 +14,12 @@ import ru.geekbrains.spacewar.base.Base2DScreen;
 public class MenuScreen extends Base2DScreen{
     SpriteBatch batch;
     Texture backgraund;
-    Texture img;
+    Texture hero;
 
     Vector2 position;
     Vector2 velocity;
 
-    Vector2 touch =
+    Vector2 touch;
 
     public MenuScreen(Game game) {
         super(game);
@@ -28,7 +29,7 @@ public class MenuScreen extends Base2DScreen{
     public void show() {
         super.show();
         batch = new SpriteBatch();
-        img = new Texture("ship.png");
+        hero = new Texture("ship.png");
         backgraund = new Texture("background.jpg");
 
         position = new Vector2(0, 0);
@@ -40,7 +41,7 @@ public class MenuScreen extends Base2DScreen{
         super.render(delta);
         batch.begin();
         batch.draw(backgraund,0,0, 1024,780);
-        batch.draw(img, position.x, position.y, 52.75f, 53f);
+        batch.draw(hero, position.x, position.y, 52.75f, 53f);
         batch.end();
         position.add(velocity);
     }
@@ -50,6 +51,19 @@ public class MenuScreen extends Base2DScreen{
         super.dispose();
         batch.dispose();
         backgraund.dispose();
-        img.dispose();
+        hero.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        super.touchDown(screenX, screenY, pointer, button);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        super.touchUp(screenX, screenY, pointer, button);
+        return false;
     }
 }
