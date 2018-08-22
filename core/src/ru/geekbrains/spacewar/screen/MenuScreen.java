562872c18efc90bc99b5350df2f170e8d6b206f0
Alexander <ka.ateist@gmail.com>
2018-08-22 13:04:14 +0300

    внес изменения следуя лекции

--- core/src/ru/geekbrains/spacewar/screen/MenuScreen.java
+++ core/src/ru/geekbrains/spacewar/screen/MenuScreen.java
@@ -3,20 +3,26 @@ package ru.geekbrains.spacewar.screen;
 import com.badlogic.gdx.Game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.spacewar.base.Base2DScreen;
-import ru.geekbrains.spacewar.base.Sprite;
 import ru.geekbrains.spacewar.math.Rect;
+import ru.geekbrains.spacewar.screen.sprites.Background;
+import ru.geekbrains.spacewar.screen.sprites.Star;
 
 /*
 *  Экран Меню
 */
 public class MenuScreen extends Base2DScreen{
-    public static final float SPEED = 1.f;
-    Texture backgraund;
-    Sprite bg;
+
+    private static final  int STAR_COUNT = 256;
+
+    private Background background;
+    private Texture bgTexture;
+    private Star star[];
+    private TextureAtlas atlas;
 
     public MenuScreen(Game game) {
         super(game);
@@ -25,30 +31,51 @@ public class MenuScreen extends Base2DScreen{
     @Override
     public void show() {
         super.show();
-
-        backgraund = new Texture("background.jpg");
-        bg = new Sprite(new TextureRegion(backgraund));
-        bg.setHeightProportion(0.25f);
+        bgTexture = new Texture("textures/background.jpg");
+        background = new Background(new TextureRegion(bgTexture));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
         }
+    }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void draw(){
         batch.begin();
-        bg.draw(batch);
+        background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
         batch.end();
+    }
+
+    public  void update(float deltaTime){
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(deltaTime);
+        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        backgraund.dispose();
-
+        bgTexture.dispose();
+        atlas.dispose();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
     }
 
     @Override
---
+++ core/src/ru/geekbrains/spacewar/screen/sprites/Star.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.spacewar.screen.sprites;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.spacewar.base.Sprite;
+import ru.geekbrains.spacewar.math.Rect;
+import ru.geekbrains.spacewar.math.Rnd;
+
+public class Star extends Sprite {
+
+    private Vector2 v = new Vector2();
+    private Rect worldBounds;
+
+    public Star(TextureAtlas atlas) {
+        super(atlas.findRegion("star"));
+        v.set(Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.005f, -0.1f));
+        setHeightProportion(0.01f);
+    }
+
+    @Override
+    public void update(float delta) {
+        pos.mulAdd(v, delta);
+    }
+
+    protected void checkAndHandleBounds(){
+        if (getRight() < worldBounds.getLeft())setLeft(worldBounds.getRight());
+        if (getLeft() < worldBounds.getRight())setRight(worldBounds.getLeft());
+        if (getTop() < worldBounds.getBottom())setBottom(worldBounds.getTop());
+
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        this.worldBounds = worldBounds;
+        float posX = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
+        float posY = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
+        pos.set(posX,posY);
+    }
+}
