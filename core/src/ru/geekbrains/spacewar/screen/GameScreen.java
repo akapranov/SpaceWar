1d1012afde0abdd6837cbd0e55baac121fb24771
Alexander <ka.ateist@gmail.com>
2018-08-22 14:59:22 +0300

    ДЗ добавил звезды (150 шт) на Игровой экран

--- core/src/ru/geekbrains/spacewar/screen/GameScreen.java
+++ core/src/ru/geekbrains/spacewar/screen/GameScreen.java
@@ -4,18 +4,24 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.geekbrains.spacewar.base.Base2DScreen;
 import ru.geekbrains.spacewar.math.Rect;
 import ru.geekbrains.spacewar.screen.sprites.Background;
+import ru.geekbrains.spacewar.screen.sprites.Star;
+
 /*
  *  Экран Игры
  */
 public class GameScreen extends Base2DScreen {
 
+    private static final int STAR_COUNT = 150;
     private Background background;
     private Texture bgTexture;
+    private Star star[];
+    private TextureAtlas atlas;
 
     public GameScreen(Game game) {
         super(game);
@@ -26,6 +32,11 @@ public class GameScreen extends Base2DScreen {
         super.show();
         bgTexture = new Texture("textures/background.jpg");
         background = new Background(new TextureRegion(bgTexture));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -38,15 +49,18 @@ public class GameScreen extends Base2DScreen {
     }
 
     public void draw() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
         batch.end();
     }
 
-    public void update(float delta) {
-
+    public void update(float deltaTime) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(deltaTime);
+        }
     }
 
     public void checkCollisions() {
@@ -61,11 +75,15 @@ public class GameScreen extends Base2DScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bgTexture.dispose();
+        atlas.dispose();
     }
 }
