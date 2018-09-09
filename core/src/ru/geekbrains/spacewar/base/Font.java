dfe3abfb823e43cd10c5ce30c746577b29abb939
Alexander <ka.ateist@gmail.com>
2018-09-09 10:39:08 +0300

    Создан класс Font

---
+++ core/src/ru/geekbrains/spacewar/base/Font.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.spacewar.base;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class Font extends BitmapFont {
+    public Font(String fontFile, String imageFile){
+        super(Gdx.files.internal(fontFile), Gdx.files.internal(imageFile), false,false);
+    }
+
+    public void setWorldSize(float worldSize){
+        getData().setScale(worldSize/getCapHeight());
+    }
+}
